52eff7a32b37e9a241a7a010c681f9ef11e55f6b
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-06-13 23:46:27 -0300

    main structure of testimonials

--- src/App.js
+++ src/App.js
@@ -4,7 +4,27 @@ import Testimony from "./components/Testimony";
 function App() {
   return (
     <div className="App">
-      <Testimony />
+      <div className="mainContainer">
+        <h1>This is what our students says:</h1>
+        <Testimony
+          name="Jennifer Karlson"
+          image="emma"
+          work="Blockchain Developer"
+          testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor odio id viverra porttitor. Duis convallis, ante id varius finibus, sapien leo lobortis elit, vitae hendrerit massa neque eget libero. In sed lobortis neque, nec gravida massa. Integer ipsum lectus, sodales et gravida in, dignissim vel dui."
+        />
+        <Testimony
+          name="Ko Kun"
+          image="shawn"
+          work="Web3 Developer"
+          testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor odio id viverra porttitor. Duis convallis, ante id varius finibus, sapien leo lobortis elit, vitae hendrerit massa neque eget libero. In sed lobortis neque, nec gravida massa. Integer ipsum lectus, sodales et gravida in, dignissim vel dui."
+        />
+        <Testimony
+          name="Carlos Don Uts"
+          image="sarah"
+          work="Metaverse Developer"
+          testimony="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor odio id viverra porttitor. Duis convallis, ante id varius finibus, sapien leo lobortis elit, vitae hendrerit massa neque eget libero. In sed lobortis neque, nec gravida massa. Integer ipsum lectus, sodales et gravida in, dignissim vel dui."
+        />
+      </div>
     </div>
   );
 }
